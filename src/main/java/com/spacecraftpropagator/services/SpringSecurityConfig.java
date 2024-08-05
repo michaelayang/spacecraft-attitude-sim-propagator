fe8834637971afbda7151cb1d47e29a9279f7245
Michael A Yang <pstester2015@gmail.com>
2024-08-05 14:32:26 -0400

    Removed unnecessary heritage code from keplerian-ptolemaic-spring-boot-exercise (Keplerian and Ptolemaic pedagogical software, also serving as a spring boot baseline exercise, in github.com/michaelayang):  CacheConfig.java.  Removed unnecessary/unused exception class RestResponseEntityExceptionHandler.java.  Pared down SpringSecurityConfig.java.

--- src/main/java/com/spacecraftpropagator/services/SpringSecurityConfig.java
+++ src/main/java/com/spacecraftpropagator/services/SpringSecurityConfig.java
@@ -17,8 +17,6 @@
 package com.spacecraftpropagator.services;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -26,30 +24,9 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-
-        auth.inMemoryAuthentication()
-                .withUser("user").password("{sha256}easy_password").roles("USER")
-                .and()
-                .withUser("admin").password("{noop}tough_password").roles("USER", "ADMIN");
-
-    }
-
-    // Secure PUT endpoints with HTTP Basic authentication (allow GET endpoints to function without authentication)
-    @Override
     protected void configure(HttpSecurity http) throws Exception {
 
-        http
-                //HTTP Basic authentication
-                .httpBasic()
-                .and()
-                .authorizeRequests()
-                .antMatchers(HttpMethod.PUT, "/loadKeplerianRecords/**").hasRole("ADMIN")
-                .antMatchers(HttpMethod.PUT, "/loadPtolemaicRecords/**").hasRole("ADMIN")
-                .antMatchers(HttpMethod.PUT, "/loadTruthDataRecords/**").hasRole("ADMIN")
-                .and()
-                .csrf().disable()
-                .formLogin().disable();
+        http.csrf().disable();
     }
 
 }
